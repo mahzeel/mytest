a5f8fb68ea40c88043d516c39be007c84dd0350f
John Chimezie <mahzeejohn@gmail.com>
2022-12-20 18:17:27 +0100

    added cart module to my code

---
+++ cypress/e2e/cart.cy.js
@@ -0,0 +1,12 @@
+import {cart} from "../../cypress/fixtures/selectors.js"
+
+describe("LAUNCH URL", function () {
+beforeEach(function () {
+    cy.visit('/')
+
+});
+
+it("I SHOULD BE ABLE TO ADD ITEMS TO CART", function () {
+
+})
+
